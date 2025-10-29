a6bb9f54770b1f2cc7720d80cdad85fce4c981b4
GumiA <alagumiran@gmail.com>
2025-10-29 11:06:38 +0800

    upd

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,13 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        window: "readonly",
+        document: "readonly"
+      }
+    },
     rules: {
       semi: ["error", "always"], // Require semicolons
       quotes: ["error", "double"], // Use double quotes
