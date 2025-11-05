dfcc12db5d5b23606e2aa133b0c31f88a2de1a66
dncnl <danielcanlapan0402@gmail.com>
2025-11-05 23:30:32 +0800

    passing test demo

--- app.test.js
+++ app.test.js
@@ -1,6 +1,24 @@
 import { jest } from "@jest/globals";
 import { showUglyUIMessage } from "./app.js";
 
+// Simple math function for testing
+function add(a, b) {
+  return a + b;
+}
+
+describe("Math Operations", () => {
+  test("adds 1 + 2 to equal 3 (passing test)", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  // This is a failing test (commented out to show the concept)
+  /* 
+  test("intentionally failing test", () => {
+    expect(true).toBe(false); // This would fail because true is not equal to false
+  });
+  */
+});
+
 describe("UI message function", () => {
   beforeAll(() => {
     global.alert = jest.fn();
